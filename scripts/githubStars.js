b0dbfc682ae47d72fac3ecef5bf21a875f130bcd
JuanPabloDiaz <juanchodis@hotmail.com>
2024-04-11 07:26:19 -0500

    Add .vercel and .env files to .gitignore, and update index.html with top banner plugin

--- scripts/githubStars.js
+++ scripts/githubStars.js
@@ -23,10 +23,13 @@ function formatNumber(number) {
 // ------------------------------ //
 
 window.onload = function () {
+  console.log("Window loaded");
+
   // Select all the links that point to a GitHub repository
   const links = Array.from(
     document.querySelectorAll('a[href^="https://github.com/"]')
   );
+  console.log("Links:", links);
   // const links = Array.from(document.querySelectorAll("https://github.com/"));
 
   links.forEach((link) => {
@@ -34,20 +37,39 @@ window.onload = function () {
     const url = new URL(link.href);
     const [owner, repo] = url.pathname.split("/").slice(1);
 
+    // // Skip the fetch operation if the current repository is the one you want to exclude
+    // if (owner === "JuanPabloDiaz" && repo === "freeForGeeks") {
+    //   return;
+    // }
+
+    console.log("Fetching data for:", owner, repo);
+
     // Make a GET request to the GitHub API's repository endpoint
-    fetch(`https://api.github.com/repos/${owner}/${repo}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Extract the stargazers_count property and display it on the site
-        const stars = data.stargazers_count;
-        console.log("stars: ", stars);
-        const Kstars = formatNumber(stars);
-        console.log("K stars: ", Kstars);
-        const starSpan = document.createElement("stars");
-        starSpan.textContent = `⭐ ${Kstars} stars`;
-        link.parentNode.insertBefore(starSpan, link.nextSibling);
+    // fetch(`https://api.github.com/repos/${owner}/${repo}`)
+    // fetch(`https://api.freeforgeeks.com/api?owner=${owner}&repo=${repo}`)
+    fetch(`https://free-for-geeks.vercel.app/api?owner=${owner}&repo=${repo}`)
+      .then((response) => {
+        console.log("Status:", response.status);
+        // console.log("Response:", response);
+        return response.text();
       })
-      .catch((error) => console.error("Error:", error));
+      .then((text) => {
+        // console.log("Data:", data);
+        // Extract the stargazers_count property and display it on the site
+        console.log("Response text:", text);
+        return JSON.parse(text);
+        // const stars = data.stars;
+        // console.log("stars: ", stars);
+        // const Kstars = formatNumber(stars);
+        // console.log("K stars: ", Kstars);
+        // const starSpan = document.createElement("stars");
+        // starSpan.textContent = `⭐ ${Kstars} stars`;
+        // link.parentNode.insertBefore(starSpan, link.nextSibling);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        console.log("Status:", error.response && error.response.status);
+      });
   });
 };
 
